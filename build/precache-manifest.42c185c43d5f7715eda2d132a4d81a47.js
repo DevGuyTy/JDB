cac5db55afd7b3b517f74e900a567c4b6ecbaa73
DevGuyTy <47014023+DevGuyTy@users.noreply.github.com>
2022-01-14 14:13:55 -0600

    Add files via upload

---
+++ build/precache-manifest.42c185c43d5f7715eda2d132a4d81a47.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29c64ae3794e3995db7bdc4cee4dabbf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d37979f9d21d227a50a2",
+    "url": "/static/css/2.a7ba36b5.chunk.css"
+  },
+  {
+    "revision": "e291228c4530a8379a74",
+    "url": "/static/css/main.a33b8191.chunk.css"
+  },
+  {
+    "revision": "d37979f9d21d227a50a2",
+    "url": "/static/js/2.e3f93694.chunk.js"
+  },
+  {
+    "revision": "50d903f5651c81ad9f9bdda00438741f",
+    "url": "/static/js/2.e3f93694.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e291228c4530a8379a74",
+    "url": "/static/js/main.c1045cbe.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  },
+  {
+    "revision": "d26b1b101ab56350401b515b84af5ea1",
+    "url": "/static/media/JDBHome.d26b1b10.webp"
+  },
+  {
+    "revision": "9deb8760104e62ec874b0c61d155e0db",
+    "url": "/static/media/JDB_Transparency.9deb8760.png"
+  },
+  {
+    "revision": "e446bf7c3847eb65a070ec5e720fddba",
+    "url": "/static/media/Padaloma.e446bf7c.ttf"
+  }
+]);
